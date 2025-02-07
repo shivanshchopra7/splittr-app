d0c9e2dda68fc18ec856da6d5bd42988ad028de7
Shivansh Chopra <gauravshivansh7@gmail.com>
2025-02-08 00:08:12 +0530

    Adding changes

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -14,7 +14,7 @@ export default function Navigation() {
           <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
         </svg>
         <span className="font-bold text-xl" style={{ color: '#6200EA' }}>
-          Split
+          Splittr
         </span>
       </Link>
 
